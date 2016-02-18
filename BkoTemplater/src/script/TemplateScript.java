f362f6890189e9af56d652af2017563cd0d94761
marcelo.c.silva <marcelo.c.silva@EISA-NB160.eisa.corp.com>
2016-02-18 11:52:43 -0200

    remoção da exteção do template script pela interface

--- BkoTemplater/src/script/TemplateScript.java
+++ BkoTemplater/src/script/TemplateScript.java
@@ -1,7 +1,15 @@
 package script;
 
-public class TemplateScript implements Template {
+public class TemplateScript{
 	String template;
+
+	public String getTemplate() {
+		return template;
+	}
+
+	public void setTemplate(String template) {
+		this.template = template;
+	}
 
 	public String toString() {
 		return this.template;
